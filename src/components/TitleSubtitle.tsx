b7e04571ae4a64b0aadd4eb14acfb5049d1b8b4a
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-05-18 22:51:56 +0530

    feat: enhance TitleSubtitle component to support action prop and improve layout

--- src/components/TitleSubtitle.tsx
+++ src/components/TitleSubtitle.tsx
@@ -1,28 +1,35 @@
 type TitleSubtitleProps = {
-  title: string;
+  title: string | React.ReactNode;
   subtitle: string;
+  action?: React.ReactNode;
   titleClassName?: string;
   subtitleClassName?: string;
+  className?: string;
 };
 
 const TitleSubtitle: React.FC<TitleSubtitleProps> = ({
   title,
   subtitle,
+  action,
   titleClassName = "",
   subtitleClassName = "",
+  className = "",
 }) => {
   return (
-    <div className="flex items-start gap-1 flex-col">
-      <h1
-        className={`xl:text-3xl text-xl font-bold text-gray-800 md:mb-2 mb-1 ${titleClassName}`}
-      >
-        {title}
-      </h1>
-      <p
-        className={`text-[#4B5563]) md:text-base font-normal text-sm ${subtitleClassName}`}
-      >
-        {subtitle}
-      </p>
+    <div className={`flex items-start justify-between gap-4 ${className}`}>
+      <div className="flex flex-col">
+        <h1
+          className={`xl:text-3xl text-xl font-bold text-gray-800 md:mb-2 mb-1 ${titleClassName}`}
+        >
+          {title}
+        </h1>
+        <p
+          className={`text-[#4B5563] md:text-base font-normal text-sm ${subtitleClassName}`}
+        >
+          {subtitle}
+        </p>
+      </div>
+      {action ? <div className="ml-2">{action}</div> : null}
     </div>
   );
 };
