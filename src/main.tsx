d71c5ca7bdfcc11b419f57737f9cb080904613e4
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-07-01 19:46:26 +0530

    feat: integrate Sentry error handling and instrumentation into React root initialization

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
+import "./instrument.ts";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
+import * as Sentry from "@sentry/react";
+
 
 import "./index.css";
 import App from "./App.tsx";
@@ -24,7 +27,19 @@ if (typeof window !== "undefined") {
   document.addEventListener("click", handleNumberInputSelection, true);
 }
 
-createRoot(document.getElementById("root")!).render(
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container, {
+  // Callback called when an error is thrown and not caught by an ErrorBoundary.
+  onUncaughtError: Sentry.reactErrorHandler((error, errorInfo) => {
+    console.warn("Uncaught error", error, errorInfo.componentStack);
+  }),
+  // Callback called when React catches an error in an ErrorBoundary.
+  onCaughtError: Sentry.reactErrorHandler(),
+  // Callback called when React automatically recovers from errors.
+  onRecoverableError: Sentry.reactErrorHandler(),
+})
+
+root.render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
