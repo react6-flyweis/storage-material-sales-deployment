c2998beba2db50b68d7b93d27fd2856e4c6115ab
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-07-01 19:46:18 +0530

    feat: integrate Sentry error tracking and global ErrorBoundary into router configuration

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,21 @@ import { Suspense } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import { salesRoutes } from "./routes/sales.routes";
 import { Loading } from "./components/loading";
+import * as Sentry from "@sentry/react";
+import ErrorBoundary from "@/pages/error-page";
 
-const router = createBrowserRouter(salesRoutes);
+
+
+const sentryCreateBrowserRouter = Sentry.wrapCreateBrowserRouterV7(createBrowserRouter);
+const router = sentryCreateBrowserRouter(salesRoutes);
 
 export function App() {
   return (
-    <Suspense fallback={<Loading />}>
-      <RouterProvider router={router} />
-    </Suspense>
+    <ErrorBoundary>
+      <Suspense fallback={<Loading />}>
+        <RouterProvider router={router} />
+      </Suspense>
+    </ErrorBoundary>
   );
 }
 
