89866f57f191d91b2fddb5c77b757ec8c517457a
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-08-07 21:06:16 +0530

    feat: export quote history and pricing rules pages from module index

--- src/modules/quotation-generator/index.ts
+++ src/modules/quotation-generator/index.ts
@@ -4,5 +4,5 @@ export * from "./layouts/quotation-sidebar";
 export * from "./pages/create-quotation-page";
 export * from "./pages/upload-drawing-page";
 export * from "./pages/extracted-drawing-page";
-export * from "./pages/quote-preview-page";
-
+export * from "./pages/quote-history-page";
+export * from "./pages/pricing-rules-page";
