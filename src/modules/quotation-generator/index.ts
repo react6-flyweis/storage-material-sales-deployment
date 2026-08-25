a657ce68a45a513d8a7279b040e04604f8a92747
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-08-25 17:50:23 +0530

    feat: export storage quote and preview pages in module index

--- src/modules/quotation-generator/index.ts
+++ src/modules/quotation-generator/index.ts
@@ -4,6 +4,8 @@ export * from "./layouts/quotation-sidebar";
 export * from "./pages/create-quotation-page";
 export * from "./pages/upload-drawing-page";
 export * from "./pages/extracted-drawing-page";
+export * from "./pages/storage-quote-page";
+export * from "./pages/storage-preview-page";
 export * from "./pages/quote-history-page";
 export * from "./pages/pricing-rules-page";
 export * from "./pricing-rules.api";
