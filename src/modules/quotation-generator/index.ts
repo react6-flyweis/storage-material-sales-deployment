033b57a3f48b20cb97c2618cdd49ebd9ab21fe5d
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-09-03 20:36:46 +0530

    feat: export EstimateDetailPage from quotation-generator module index

--- src/modules/quotation-generator/index.ts
+++ src/modules/quotation-generator/index.ts
@@ -6,6 +6,7 @@ export * from "./pages/pemb-quote-page";
 export * from "./pages/storage-quote-page";
 export * from "./pages/storage-preview-page";
 export * from "./pages/quote-history-page";
+export * from "./pages/estimate-details-page";
 export * from "./pages/quote-preview-page";
 export * from "./pages/quote-preview-list-page";
 export * from "./pages/pricing-rules-page";
